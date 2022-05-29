fabfbdae19ec2346b3bf945a113f03175482c97b
HajarAlnasser <86411115+HajarAlnasser@users.noreply.github.com>
2022-05-29 21:50:25 +0300

    some changes just in case

--- Savy GameJam 2D/Assets/TheColorsList.cs
+++ Savy GameJam 2D/Assets/TheColorsList.cs
@@ -23,24 +23,34 @@ public class TheColorsList : MonoBehaviour
     public void red()
     {
         nailpolishColor.color = Color.red;
-        StartGame();
-    }
-
-    public void green()
-    {
-        nailpolishColor.color = new Color(0.4065498f, 0.7830189f, 0.3870772f, 1f);
-        StartGame();
-    }
-    public void blue()
-    {
-        nailpolishColor.color = new Color(0.3212531f, 0.8526323f, 0.8867924f, 1f);
-        StartGame();
+        
     }
 
     public void pink()
     {
-        nailpolishColor.color = new Color(0.9433962f, 0.2883588f, 0.6524814f, 1f);
-        StartGame();
+        nailpolishColor.color = new Color(0.6509434f, 0.3960929f, 0.5295146f, 1f);
+        
+    }
+    public void green()
+    {
+        nailpolishColor.color = new Color(0.3019313f, 0.5566038f, 0.3892476f, 1f);
+
+    }
+    public void skyBlue()
+    {
+        nailpolishColor.color = new Color(0.0473478f, 0.5182636f, 0.5283019f, 1f);
+        
+    }
+
+    public void black()
+    {
+        nailpolishColor.color = new Color(0f, 0f, 0f, 1f);
+        
+    }
+    public void Purbol()
+    {
+        nailpolishColor.color = new Color(0.4445616f, 0.2382075f, 0.5f, 1f);
+        
     }
 
     private void StartGame()
